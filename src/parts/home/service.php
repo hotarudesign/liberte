a59e998d85e17e66c73224593c1323c0f22ec3df
hotarudesign <hotarudesign1220@gmail.com>
2023-09-02 05:56:44 +0900

    💄 serviceセクションの調整

--- src/parts/home/service.php
+++ src/parts/home/service.php
@@ -8,10 +8,16 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div class="swiper__txt">
-                    <h3 class="swiper__txt-ttl">クラウドファンディング事業</h3>
-                    <p class="swiper__txt-desc">
-                        これまで日本国内で5億円を超える商品を流通させた経験をもとに、日本国内に進出したい海外企業のお手伝いを得意としています。具体的には1億人規模の大きな日本市場に向けたテストマーケティングの一環として、海外メーカーとタッグを組んだクラウドファンディング事業を行っています。クラウドファンデングの後は国内有名ショッピングモールへの出店や、展示会への出店も通じ、継続的に日本市場に向けたアプローチもご提案します。
-                    </p>
+                    <h3 class="swiper__txt-ttl">クラウドファンディング事業<span class="swiper__txt-ttl-en">Crowdfunding Business
+                        </span></h3>
+                    <div class="swiper__txt__desc">
+                        <p class="swiper__txt__desc-ja">
+                            日本国内で年間3億円ほどの売上を作っています。その経験をもとに、海外メーカーの総代理店としてクラウドファンディングを実施します。クラウドファンディングはテストマーケティングになります。実績ある弊社と日本に進出しましょう。
+                        </p>
+                        <p class="swiper__txt__desc-en">
+                            Our annual sales in Japan are about 300 million yen. Based on this experience, we will conduct crowdfunding as the sole distributor for overseas manufacturers. Crowdfunding is test marketing. Let's enter Japan with our proven company.
+                        </p>
+                    </div>
                 </div>
                 <figure class="swiper__thumb">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webp/service_item01.webp" alt="" />
@@ -19,10 +25,16 @@
             </div>
             <div class="swiper-slide">
                 <div class="swiper__txt">
-                    <h3 class="swiper__txt-ttl">EC事業</h3>
-                    <p class="swiper__txt-desc">
-                        日本Amazon、Yahooショッピングを主な販路とし、国内最大規模のプラットフォームにて海外製品の流通販売に取り組んでおります。またアメリカAmazon、東南アジア最大のECモール「Shopee」などに出店しております。海外商品を日本国内に流通させることはもちろんのこと、その後の展開も見据えた戦略にて、売上の最大化を支援しています。
-                    </p>
+                    <h3 class="swiper__txt-ttl">EC事業<span class="swiper__txt-ttl-en">E-commerce Business
+                        </span></h3>
+                    <div class="swiper__txt__desc">
+                        <p class="swiper__txt__desc-ja">
+                            日本Amazon、楽天、Yahoo!ショッピングを主な販路としています。国内最大規模のプラットフォームにて、海外メーカー商品の販売を代理で行います。クラウドファンディング後は、日本のオンライン市場に展開し、売上を伸ばしましょう。
+                        </p>
+                        <p class="swiper__txt__desc-en">
+                            Our main sales channels are Amazon Japan, Rakuten, Yahoo! We will sell products from foreign manufacturers on behalf of you on the largest platform in Japan. After crowdfunding, expand into the Japanese online market and increase your sales.
+                        </p>
+                    </div>
                 </div>
                 <figure class="swiper__thumb">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webp/service_item02.webp" alt="" />
@@ -31,9 +43,14 @@
             <div class="swiper-slide">
                 <div class="swiper__txt">
                     <h3 class="swiper__txt-ttl">WEB広告運用事業</h3>
-                    <p class="swiper__txt-desc">
-                        Google広告、Facebook広告の運用も行なっています。日本国内に流通させたい商品の売上最大化にあたり、見込み客へ効率的にアプローチするための集客方法としての広告運用方法を提案、代行しております。
-                    </p>
+                    <div class="swiper__txt__desc">
+                        <p class="swiper__txt__desc-ja">
+                            Google広告、Facebook広告の運用も行なっています。日本国内に流通させたい商品の売上最大化にあたり、見込み客へ効率的にアプローチするための集客方法としての広告運用方法を提案、代行しております。
+                        </p>
+                        <p class="swiper__txt__desc-ja">
+
+                        </p>
+                    </div>
                 </div>
                 <figure class="swiper__thumb">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webp/service_item03.webp" alt="" />
@@ -41,10 +58,15 @@
             </div>
             <div class="swiper-slide">
                 <div class="swiper__txt">
-                    <h3 class="swiper__txt-ttl">SNSマーケティング事業</h3>
-                    <p class="swiper__txt-desc">
-                        主にInstagramを利用したマーケティング事業も行なっております。日本国内に流通させたい商品の売上最大化にあたり、見込み客へ効率的にアプローチするための集客方法としてのSNS運用方法を提案、代行しております。
-                    </p>
+                    <h3 class="swiper__txt-ttl">SNSマーケティング事業<span class="swiper__txt-ttl-en"></span></h3>
+                    <div class="swiper__txt__desc">
+                        <p class="swiper__txt__desc-ja">
+                            主にInstagramを利用したマーケティング事業も行なっております。日本国内に流通させたい商品の売上最大化にあたり、見込み客へ効率的にアプローチするための集客方法としてのSNS運用方法を提案、代行しております。
+                        </p>
+                        <p class="swiper__txt__desc-en">
+
+                        </p>
+                    </div>
                 </div>
                 <figure class="swiper__thumb">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webp/service_item04.webp" alt="" />
@@ -52,10 +74,15 @@
             </div>
             <div class="swiper-slide">
                 <div class="swiper__txt">
-                    <h3 class="swiper__txt-ttl">コンサルティング</h3>
-                    <p class="swiper__txt-desc">
-                        新たにEC事業に取り組まれたい方に向けたマンツーマンのコンサルティングを提供しています。競合他社分析にはじまる市場調査から、オンラインでの自社商品のリリースから流通までの提案と実践を支援します。法人の方から個人の方まで、弊社ビジョンに共感いただけるお客様のお手伝いも6年ほどになりました。すでにEC事業を展開されている方も、ゼロからスタートされる方もまずはお気軽にお問合せください。
-                    </p>
+                    <h3 class="swiper__txt-ttl">コンサルティング<span class="swiper__txt-ttl-en"></span></h3>
+                    <div class="swiper__txt__desc">
+                        <p class="swiper__txt__desc-ja">
+                            新たにEC事業に取り組まれたい方に向けたマンツーマンのコンサルティングを提供しています。競合他社分析にはじまる市場調査から、オンラインでの自社商品のリリースから流通までの提案と実践を支援します。法人の方から個人の方まで、弊社ビジョンに共感いただけるお客様のお手伝いも6年ほどになりました。すでにEC事業を展開されている方も、ゼロからスタートされる方もまずはお気軽にお問合せください。
+                        </p>
+                        <p class="swiper__txt__desc-en">
+
+                        </p>
+                    </div>
                 </div>
                 <figure class="swiper__thumb">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webp/service_item05.webp" alt="" />
